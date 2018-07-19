9cb7038d8ebcc7ed93260d5cd533bbfa749400f3
joaopedroa.6 <joaopedroa.6@hotmail.com>
2018-07-18 21:17:12 -0300

    Slides Button Banco de Dados

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,36 +11,43 @@ import { Observable } from 'rxjs/Observable';
 export class HomePage {
 
   itensBasicos: Observable<any[]>;
+  itensBasicosFilter = {};
   base = 'base';
   nivelArvore:string = 'base';
   constructor(public navCtrl: NavController,public database:AngularFireDatabase) {
 
     
-    this.itensBasicos = this.database.list(this.nivelArvore).snapshotChanges().map(arr => {
-          return arr.map(snap => Object.assign(snap.payload.val(), { $key: snap.key }) )
+    this.itensBasicos = this.database.list(this.nivelArvore + '/-LHjj5IDSxQaZ89wAL6p').snapshotChanges().map(arr => {
+          return arr.map(snap => Object.assign(snap.payload.val(), { $key: snap.key }) ).filter(i => i.$key !== 'dado' && i.$key !== 'final' && i.$key !== 'tipo')
     });
 
-      this.itensBasicos.forEach(e=>{
-        console.log(e);
-      })
-   
-
+    this.itensBasicos. forEach(e =>{
+      //console.log(Object.getOwnPropertyNames(e[0])); 
+      console.log(e); 
+    });
+  
+  
   }
 
   entrarNivel(key:string){
     this.nivelArvore = this.nivelArvore + '/' + key;
     console.log(this.nivelArvore);
     this.itensBasicos = this.database.list(this.nivelArvore).snapshotChanges().map(arr => {
-      return arr.map(snap => Object.assign(snap.payload.val(), { $key: snap.key }) )
+      return arr.map(snap => Object.assign(snap.payload.val(), { $key: snap.key }) ).filter(i => i.$key !== 'dado' && i.$key !== 'final' && i.$key !== 'tipo')
     });
+  
 
   }
   sairNivel(){
     let arrayPath = [];
     arrayPath = this.nivelArvore.split('/');
     arrayPath.splice(-1,1);
+    this.nivelArvore = arrayPath.toString().replace(',','/');
+    this.itensBasicos = this.database.list(this.nivelArvore).snapshotChanges().map(arr => {
+      return arr.map(snap => Object.assign(snap.payload.val(), { $key: snap.key }) ).filter(i => i.$key !== 'dado' && i.$key !== 'final' && i.$key !== 'tipo')
+    });
+   
     
-    console.log(arrayPath.toString().replace(',','/'));
   }
 
 }
